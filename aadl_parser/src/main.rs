6abdae7b89712308f2af6706576d1fdd89e38150
chengjian646 <2280896448@qq.com>
2025-05-22 18:25:53 +0800

    Change to read AADL files

--- aadl_parser/src/main.rs
+++ aadl_parser/src/main.rs
@@ -2,6 +2,7 @@ pub mod aadlight_parser;
 
 use aadlight_parser::AADLParser;
 use pest::{iterators::Pair, Parser};
+use std::fs;
 
 // fn print_pair(pair: Pair<aadlight_parser::Rule>, indent: usize) {
 //     println!(
@@ -50,127 +51,22 @@ fn print_pair(pair: Pair<aadlight_parser::Rule>, indent: usize) {
     }
 }
 fn main() {
-    let aadl_input = r#"
-        package ojr_pingpong::queued
-public
-with Data_Model;
-pingpong renames package ojrpingpong::queued;
-  system root
-  end root;
-
-  system implementation root.impl
-  subcomponents
-    the_cpu: processor cpu.impl;
-    the_proc: process proc.impl;
-    the_mem: memory mem.impl;
-  end root.impl;
-
-  processor cpu
-  end cpu;
-
-  processor implementation cpu.impl
-  end cpu.impl;
-
-
-
-  process proc
-  end proc;
-
-  process implementation proc.impl
-  subcomponents
-    the_sender: thread sender.impl;
-    the_receiver: thread receiver.impl;
-  connections
-    cnx: port the_sender.p -> the_receiver.p;
-  end proc.impl;
-
-  memory mem
-  end mem;
-
-  memory implementation mem.impl
-  properties
-    Memory_Size => 200 KByte;
-  end mem.impl;
-
-  thread sender
-  features
-    p: out event data port Integer;
-  properties
-    Dispatch_Protocol => Periodic;
-    Compute_Execution_Time => 0 ms .. 1 ms;
-    Period => 2000 Ms;
-    Priority => 5;
-    Data_Size => 40000 bytes;
-    Stack_Size => 40000 bytes;
-    Code_Size => 40 bytes;
-  end sender;
-
-  thread implementation sender.impl
-  calls
-    call : { c : subprogram sender_spg;};
-  connections
-    cnx: parameter c.result -> p;
-  properties
-    Compute_Entrypoint_Call_Sequence => reference (call);
-  end sender.impl;
-
-  subprogram sender_spg
-  features
-    result : out parameter Integer;
-  properties 
-  	source_text => ("");
-    source_name => "PingPong.Send"; 
-    source_language => (java);
-  end sender_spg;
-
-  thread receiver
-  features
-    p: in event data port Integer;
-  properties
-    Dispatch_Protocol => Periodic;
-    Compute_Execution_Time => 0 ms .. 1 ms;
-    Period => 1000 Ms;
-    Priority => 10;
-    Data_Size => 40000 bytes;
-    Stack_Size => 40000 bytes;
-    Code_Size => 40 bytes;
-  end receiver;
-
-  thread implementation receiver.impl
-  calls
-    call : { c : subprogram receiver_spg;};
-  connections
-    cnx: parameter p -> c.input;
-  properties
-    Compute_Entrypoint_Call_Sequence => reference (call);
-  end receiver.impl;
-
-  subprogram receiver_spg
-  features
-    input : in parameter Integer;
-  properties 
-  	source_text => ("");
-    source_name => "PingPong.Receive"; 
-    source_language => (java); 
-  end receiver_spg;
-
-  data Integer
-  properties
-    Source_Name => "PingPongType"; 
-    Data_Model::Initial_Value => ("new PingPongType()");
-  end Integer;
-	
-end ojr_pingpong_queued;
-    "#;
-
-    match AADLParser::parse(aadlight_parser::Rule::file, aadl_input) {
+    let path = "pingpong.aadl";
+    let aadl_input = match fs::read_to_string(path) {
+        Ok(content) => content,
+        Err(err) => {
+            eprintln!("读取文件失败: {}", err);
+            return;
+        }
+    };
+    match AADLParser::parse(aadlight_parser::Rule::file, &aadl_input) {
         Ok(pairs) => {
             for pair in pairs {
                 print_pair(pair, 0);
             }
         }
         Err(e) => {
-            eprintln!("Parse error: {}", e);
+            eprintln!("解析失败: {}", e);
         }
     }
 }
