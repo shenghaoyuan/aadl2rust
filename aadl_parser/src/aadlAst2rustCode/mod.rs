35d20ec5b96fdd6335ca5b0abe8cf9ea2fa2acba
chengjian646 <2280896448@qq.com>
2025-09-12 19:47:13 +0800

    fix1

--- aadl_parser/src/aadlAst2rustCode/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod converter;
-pub mod converter_annex;
-pub mod intermediate_ast;
-pub mod intermediate_print;
-pub mod merge_utils;
-pub mod generate_build;
--- aadl_parser/src/aadlight_parser.rs
+++
@@ -1,5 +0,0 @@
-use pest_derive::Parser;
-
-#[derive(Parser)]
-#[grammar = "aadl.pest"]
-pub struct AADLParser;
